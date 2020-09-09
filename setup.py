4bfb1e1748acc4f7ae359d046fdef2431d5f9f41
alex <geniofuturo@gmail.com>
2020-09-09 12:02:12 -0300

    Intial package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='xadmin-generic-search',
+    version='1.0.0',
+    packages=['xplugin_generic_search'],
+    url='https://github.com/alexsilva/xadmin-generic-search',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    include_package_data=True,
+    description='Plugin that adds the ability to search for generic content (with content-type).'
+)
