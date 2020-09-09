41724266b1f13dda4d75785fff489df414c5e939
alex <geniofuturo@gmail.com>
2020-09-09 16:33:42 -0300

    Search related plugin.

---
+++ xplugin_generic_search/plugin.py
@@ -0,0 +1,45 @@
+from xadmin.filters import SEARCH_VAR
+from xadmin.plugins.utils import get_context_dict
+from xadmin.views import BaseAdminPlugin
+from xadmin.views import ListAdminView
+from django import template
+from xplugin_generic_search.search import GenericSearchMixin
+
+
+class GenericSearchPlugin(BaseAdminPlugin):
+    related_search_fields = ()
+    related_search_mapping = {}
+
+    def init_request(self, *args, **kwargs):
+        return isinstance(self.admin_view, ListAdminView)
+
+    def do_search(self, search_query, queryset):
+        search_mixin = GenericSearchMixin(self.model,
+                                          self.related_search_fields,
+                                          self.related_search_mapping)
+        queryset, use_distinct = search_mixin.get_results(search_query, queryset)
+        if use_distinct:
+            queryset = queryset.distinct()
+        return queryset
+
+    def get_list_queryset(self, queryset):
+        search_query = self.request.GET.get(SEARCH_VAR, '')
+        if search_query and self.related_search_fields:
+            queryset = self.do_search(search_query, queryset)
+            self.admin_view.search_query = search_query
+        return queryset
+
+    def block_nav_form(self, context, nodes):
+        if self.related_search_fields:
+            context = get_context_dict(context or {})  # no error!
+            self.admin_view.search_fields = self.related_search_fields
+            context.update({
+                'search_var': SEARCH_VAR,
+                'remove_search_url': self.admin_view.get_query_string(remove=[SEARCH_VAR]),
+                'search_form_params': self.admin_view.get_form_params(remove=[SEARCH_VAR])
+            })
+            nodes.append(
+                template.loader.render_to_string(
+                    'xadmin/blocks/model_list.nav_form.search_form.html',
+                    context=context)
+            )
