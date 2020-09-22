9ec123d22fdf4feb552f73f38019ee4b20e5686e
alex <geniofuturo@gmail.com>
2020-09-22 17:29:08 -0300

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xadmin-generic-search',
-    version='1.0.0',
+    version='1.1.0',
     packages=['xplugin_generic_search'],
     url='https://github.com/alexsilva/xadmin-generic-search',
     license='MIT',
